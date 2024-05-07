c9cba782fa70d9b2bea45b9394a903c06252a562
makoge boris <makogeboris@gmail.com>
2024-05-07 10:00:11 +0100

    modified

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51354" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "21336" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
