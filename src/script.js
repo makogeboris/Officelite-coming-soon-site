84b5ca6c06ce248749f114d790e51f7401d873e7
makoge boris <makogeboris@gmail.com>
2024-05-15 14:14:09 +0100

    modified

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+import dayjs from "dayjs";
+
+const relativeTime = require("dayjs/plugin/relativeTime");
+const duration = require("dayjs/plugin/duration");
+
+dayjs.extend(relativeTime);
+dayjs.extend(duration);
+
+const future = document.querySelectorAll(".countdown-heading__span");
+const days = document.querySelectorAll(".days");
+const hours = document.querySelectorAll(".hours");
+const minutes = document.querySelectorAll(".minutes");
+const seconds = document.querySelectorAll(".seconds");
+
+const todayDate = dayjs();
+const futureDate = todayDate.add(30, "day");
+
+future.forEach((day) => {
+  day.textContent = futureDate.format("DD MMM YYYY");
+});
+
+const daysFromDate = futureDate.diff(todayDate, "day");
+const hoursFromDate = futureDate.diff(todayDate, "hour");
+
+console.log(daysFromDate);
+console.log(hoursFromDate);
+
+days.forEach((day) => {
+  day.textContent = daysFromDate;
+});
+
+hours.forEach((hour) => {
+  hour.textContent = hoursFromDate;
+});
